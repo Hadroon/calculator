bfafc9c33fb4a616d749b9d9263fa803d758e141
Hadroon <gabor.muranyi@gmail.com>
2022-04-20 15:18:51 +0200

    setup

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '/index.html'));
+  });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
+// "scripts": {
+//   "serve": "cd ui & npm run build & cd .. & node main.js",
+//   "dev": "supervisor  main.js"
+//   },
