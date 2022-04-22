04c83fe264c9f16c4fddbaefc094f0b11881bcd6
Hadroon <gabor.muranyi@gmail.com>
2022-04-22 10:23:20 +0200

    basic calculator design

--- client/src/components/calculator.vue
+++ client/src/components/calculator.vue
@@ -1,6 +1,31 @@
 <template>
-  <div>
-    <p>Hello calculator</p>
+  <div class="main-wrapper">
+    <div class="display">
+    </div>
+    <div class="container">
+      <div class="button">MC</div>
+      <div class="button">MR</div>
+      <div class="button">M-</div>
+      <div class="button">M+</div>
+      <div class="button">7</div>
+      <div class="button">8</div>
+      <div class="button">9</div>
+      <div class="button">÷</div>
+      <div class="button">4</div>
+      <div class="button">5</div>
+      <div class="button">6</div>
+      <div class="button">*</div>
+      <div class="button">1</div>
+      <div class="button">2</div>
+      <div class="button">3</div>
+      <div class="button">-</div>
+      <div class="button">00</div>
+      <div class="button">0</div>
+      <div class="button">.</div>
+      <div class="button">+</div>
+      <div class="button clear-button">C</div>
+      <div class="button equal-button">=</div>
+    </div>
   </div>
 </template>
 
